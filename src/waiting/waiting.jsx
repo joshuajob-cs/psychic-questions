6328d39298123ef5e2ebad79030f2f9a997da66b
Joshua Job <joshuasjob@hotmail.com>
2026-02-12 14:17:06 -0700

    Add header and footer to pages

--- src/waiting/waiting.jsx
+++ src/waiting/waiting.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { Title } from "../join/title";
+import { Footer } from "../join/shared-footer";
 
 export function Waiting() {
   return (
-    <main>
-      <h1>Waiting...</h1>
-      <img
-        src="MysteriousWalking.jpg"
-        width="500"
-        alt="A person walking into a foggy, mysterious mist"
-      />
-    </main>
+    <>
+      <header>
+        <Title />
+      </header>
+      <main>
+        <h1>Waiting...</h1>
+        <img
+          src="MysteriousWalking.jpg"
+          width="500"
+          alt="A person walking into a foggy, mysterious mist"
+        />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
