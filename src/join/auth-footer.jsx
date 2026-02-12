722ae1479430c563db9e77dedc304c1ad87094d5
Joshua Job <joshuasjob@hotmail.com>
2026-02-12 13:15:46 -0700

    Footers can differ between pages

---
+++ src/join/auth-footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Footer } from "./shared-footer";
+
+export function AuthFooter() {
+  return (
+    <footer>
+      <h2>
+        <a id="github-link" href="https://github.com/joshuajob-cs/startup">
+          GitHub
+        </a>
+      </h2>
+      <Footer />
+    </footer>
+  );
+}
---
+++ src/join/title.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Title() {
+  return <h3>🔮 Psychic Questions 🤔</h3>;
+}
