166c3353ac9fa2f9d42fb8261ad17acf153afa49
Joshua Job <joshuasjob@hotmail.com>
2026-02-11 22:23:12 -0700

    First page has only header and footer

--- src/app.jsx
+++ src/app.jsx
@@ -34,24 +34,7 @@ export default function App() {
           </div>
         </nav>
       </header>
-      <main>
-        <div className="screen-rotater">
-          <div>
-            <h1>Join Game</h1>
-            <form action="HTML/enter-name.html">
-              <input type="text" placeholder="Join Code" required />
-              <div>
-                <button type="submit">Join</button>
-              </div>
-            </form>
-          </div>
-          <img
-            src="HTML/Brain.png"
-            width="300"
-            alt="You climb up a latter to reach a brain"
-          />
-        </div>
-      </main>
+      <main>App content goes here</main>
       <footer>
         <h2>
           <a id="github-link" href="https://github.com/joshuajob-cs/startup">
