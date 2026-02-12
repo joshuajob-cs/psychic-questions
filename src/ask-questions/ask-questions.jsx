68865c850b47268b2c7715724abddb1024123a75
Joshua Job <joshuasjob@hotmail.com>
2026-02-12 14:20:22 -0700

    Add header and footer to pages

--- src/ask-questions/ask-questions.jsx
+++ src/ask-questions/ask-questions.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "./ask-questions.css";
+import { Title } from "../join/title";
+import { Footer } from "../join/shared-footer";
 
 export function AskQuestions() {
   return (
-    <main>
-      <div className="container">
-        <h2 className="basic-font">Your Questions</h2>
-        <h1>Who do you aspire to be like?</h1>
-        <form action="/guess-answers">
-          <textarea placeholder="Enter your answer here..."></textarea>
-          <div>Q: 1/4</div>
-          <div className="button-container">
-            <button>Next →</button>
-          </div>
-        </form>
-      </div>
-    </main>
+    <>
+      <header>
+        <Title />
+      </header>
+      <main>
+        <div className="container">
+          <h2 className="basic-font">Your Questions</h2>
+          <h1>Who do you aspire to be like?</h1>
+          <form action="/guess-answers">
+            <textarea placeholder="Enter your answer here..."></textarea>
+            <div>Q: 1/4</div>
+            <div className="button-container">
+              <button>Next →</button>
+            </div>
+          </form>
+        </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
--- src/winner/winner.jsx
+++ src/winner/winner.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { Title } from "../join/title";
+import { Footer } from "../join/shared-footer";
 
 export function Winner() {
   return (
-    <main>
-      <h1>The winner is Jenny!</h1>
-      <h2>With 95 points</h2>
-      <img
-        src="Elephant.png"
-        width="300"
-        alt="An adorable elephant celebrates your victory"
-      />
-    </main>
+    <>
+      <header>
+        <Title />
+      </header>
+      <main>
+        <h1>The winner is Jenny!</h1>
+        <h2>With 95 points</h2>
+        <img
+          src="Elephant.png"
+          width="300"
+          alt="An adorable elephant celebrates your victory"
+        />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
