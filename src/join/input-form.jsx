7c617ecb7cadd2204391f8cb804d4ff585063770
Joshua Job <joshuasjob@hotmail.com>
2026-02-26 21:51:41 -0700

    Takes a function to check if input is valid

--- src/join/input-form.jsx
+++ src/join/input-form.jsx
@@ -2,18 +2,21 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function InputForm({
-  inputSpecs, // [{ name, type, placeholder }]
+  inputSpecs, // Specifications for the input field (type, placeholder, etc.)
   successRoute, // path to navigate on success
   errorMessage, // message to show if validate fails
   buttonText, // text for submit button
+  validate, // function to validate the input
 }) {
   const [badEntry, setBadEntry] = useState(false);
+  const [input, setInput] = useState("");
   const go = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const isValid = validate ? validate(input) : true;
 
-    if (true) {
+    if (isValid) {
       go(successRoute);
     } else {
       setBadEntry(true);
@@ -24,8 +27,10 @@ export function InputForm({
     <form onSubmit={handleSubmit}>
       <div key={inputSpecs.name}>
         <input
+          name={inputSpecs.name}
           type={inputSpecs.type}
           placeholder={inputSpecs.placeholder}
+          onChange={(e) => setInput(e.target.value)}
           required
         />
       </div>
