a82a799d30dcc60b6102957e5740f60b22b0ff04
Joshua Job <joshuasjob@hotmail.com>
2026-02-26 22:50:56 -0700

    input-tester for join works again

--- src/join/input-tester.jsx
+++ src/join/input-tester.jsx
@@ -5,15 +5,17 @@ export function InputTester() {
   return (
     <>
       <InputForm
-        inputSpecs={{
-          name: "joinCode",
-          type: "text",
-          placeholder: "Join Code",
-        }}
+        inputSpecs={[
+          {
+            name: "joinCode",
+            type: "text",
+            placeholder: "Join Code",
+          },
+        ]}
         successRoute="/enter-name"
         errorMessage="Invalid join code. Correct code is ABCDE."
         buttonText="Join"
-        validate={(code) => code === "ABCDE"}
+        validate={(inputs) => inputs.joinCode === "ABCDE"}
       />
     </>
   );
