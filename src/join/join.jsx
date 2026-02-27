b039dca3acaaa008a5cd4cbcbb6859f66aea710c
Joshua Job <joshuasjob@hotmail.com>
2026-02-26 22:11:16 -0700

    Refactored input-tester to rely on more general input-form

--- src/join/join.jsx
+++ src/join/join.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { AuthHeader } from "./auth-header";
 import { AuthFooter } from "./auth-footer";
 import { InputTester } from "./input-tester";
+import { InputTester2 } from "./input-tester2";
 import "./join.css";
 
 export function Join() {
@@ -12,7 +13,7 @@ export function Join() {
         <div className="screen-rotater" id="join-rotater">
           <div>
             <h1>Join Game</h1>
-            <InputTester />
+            <InputTester2 />
           </div>
           <img
             id="shrinkable"
