a1268b9eb00c170ef0e32ab4714dfe659fc4128d
Joshua Job <joshuasjob@hotmail.com>
2026-02-26 23:26:07 -0700

    Mostly works in login input-tester, just won't submit

--- src/login/input-tester.jsx
+++ src/login/input-tester.jsx
@@ -1,51 +1,24 @@
 import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { InputForm } from "../join/input-form";
 
 export function InputTester() {
-  const [badEntry, setBadEntry] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const go = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (username === "john" && password === "doe") {
-      go("/enter-name");
-    } else {
-      setBadEntry(true);
-    }
-  };
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            required
-          />
-          {badEntry && (
-            <p style={{ color: "red" }}>
-              Invalid username or password. (Hint: the correct username is
-              "john" and the correct password is "doe")
-            </p>
-          )}
-        </div>
-        <div>
-          <button type="submit">Login</button>
-        </div>
-      </form>
+      <InputForm
+        inputSpecs={[
+          { name: "username", type: "text", placeholder: "Username" },
+          { name: "password", type: "password", placeholder: "Password" },
+        ]}
+        successRoute="/enter-name"
+        buttonText="Login"
+        errorMessage='Invalid username or password. (Hint: username "john", password "doe")'
+        validate={(inputs) =>
+          inputs.username === "john" && inputs.password === "doe"
+        }
+        save={(inputs) =>
+          setUser((prevUser) => ({ ...prevUser, username: inputs.username }))
+        }
+      />
     </>
   );
 }
