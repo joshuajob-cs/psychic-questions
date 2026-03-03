4eeffd6783ed8a0271eea908278d980773bc1f3c
Joshua Job <joshuasjob@hotmail.com>
2026-03-03 02:36:26 -0700

    Last Question is different

--- src/guess-answers/guess-answers.jsx
+++ src/guess-answers/guess-answers.jsx
@@ -49,9 +49,13 @@ export function GuessAnswers() {
         {questions.slice(0, quesIndex).map((question, i) => (
           <div className="question" key={i}>
             <div className="fancy-font">{question}</div>
-            <div className="answer">
-              Answer: {possibleAnswers[i][correctAnswers[i]]}
-            </div>
+            {i < quesIndex - 1 ? (
+              <div className="answer">
+                Answer: {possibleAnswers[i][correctAnswers[i]]}
+              </div>
+            ) : (
+              <div className="answer-choices">{}</div>
+            )}
           </div>
         ))}
         {quesIndex < questions.length && (
