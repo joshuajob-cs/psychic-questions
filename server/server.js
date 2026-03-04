a69e942856f80dcd9a35c79a60384322d62c6a4b
Joshua Job <joshuasjob@hotmail.com>
2026-03-03 18:21:58 -0700

    Added login endpoint

--- server/server.js
+++ server/server.js
@@ -11,6 +11,16 @@ app.post("/auth/create", (req, res) => {
   res.send({ username });
 });
 
+app.post("/auth/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((nextUser) => nextUser.username === username);
+  if (user && user.password === password) {
+    res.send({ username });
+  } else {
+    res.status(401).send({ msg: "Unauthorized" });
+  }
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
