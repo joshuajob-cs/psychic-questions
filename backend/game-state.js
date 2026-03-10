ce677f83e6a34de605c9584e3e407e5bbd701a24
Joshua Job <joshuasjob@hotmail.com>
2026-03-09 22:46:01 -0600

    Factored out game-state, so it can be used by both game-service, and question service

---
+++ backend/game-state.js
@@ -0,0 +1,26 @@
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.points = 0;
+    this.questions = [];
+  }
+
+  addQuestion(question, answer) {
+    this.questions.push({ question, answer });
+  }
+}
+
+class Game {
+  constructor(gameCode) {
+    this.gameCode = gameCode;
+    this.players = {};
+  }
+
+  addPlayer(username, name) {
+    this.players[username] = new Player(name);
+  }
+}
+
+const games = {};
+
+module.exports = { games, Game, Player };
