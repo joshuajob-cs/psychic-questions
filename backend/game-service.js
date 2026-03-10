6ad6934af480459dde99465576a710b20865f1c0
Joshua Job <joshuasjob@hotmail.com>
2026-03-09 22:36:48 -0600

    Created Player class

--- backend/game-service.js
+++ backend/game-service.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.points = 0;
+    this.questions = [];
+  }
+
+  addQuestion(text, answer) {
+    this.questions.push({ question, answer });
+  }
+}
+
+const players = {};
+
 module.exports = router;
