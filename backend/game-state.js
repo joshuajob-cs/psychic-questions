5fb4ce13959f48ee26851eedc666b344eac6ad0a
Joshua Job <joshuasjob@hotmail.com>
2026-03-10 01:21:11 -0600

    getWinner function

--- backend/game-state.js
+++ backend/game-state.js
@@ -31,6 +31,12 @@ class Game {
     this.players[name].points += delta;
     return true;
   }
+
+  getWinner() {
+    const players = Object.values(this.players);
+    if (players.length === 0) return null;
+    return players.reduce((a, b) => (a.points >= b.points ? a : b));
+  }
 }
 
 const games = {};
