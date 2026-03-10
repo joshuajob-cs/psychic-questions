bc0a801c66ed84e8b75136091ee47142a1ce017c
Joshua Job <joshuasjob@hotmail.com>
2026-03-10 10:40:28 -0600

    index.js

---
+++ service/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+import authService from "./auth-service.js";
+import gameService from "./game-service.js";
+import questionService from "./question-service.js";
+
+const app = express();
+
+app.use(express.static("../dist"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/auth", authService);
+app.use("/game", gameService);
+app.use("/question", questionService);
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
