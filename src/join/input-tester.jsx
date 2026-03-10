1f569d5d8c5d4aaccfcd3ddfce73aa43f2b898c7
Joshua Job <joshuasjob@hotmail.com>
2026-03-10 06:28:08 -0600

    Join checks for error in input-tester

--- src/join/input-tester.jsx
+++ src/join/input-tester.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { InputForm } from "../components/input-form";
 import { Context } from "../context";
 import { checkGame } from "../storage-api/game-api";
@@ -16,9 +16,15 @@ export function InputTester() {
           },
         ]}
         successRoute="/enter-name"
-        errorMessage="Invalid join code."
         buttonText="Join"
-        validate={async (inputs) => await checkGame(inputs.joinCode)}
+        validate={async (inputs) => {
+          try {
+            await checkGame(inputs.joinCode);
+            return true;
+          } catch (err) {
+            return err.message;
+          }
+        }}
         save={(inputs) => setUser({ ...user, joinCode: inputs.joinCode, score: 0 })}
       />
     </>
