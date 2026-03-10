e7fd648d7da08ba12e0d5384fc2a5bea223947ba
Joshua Job <joshuasjob@hotmail.com>
2026-03-10 17:36:03 -0600

    guess-answers gets a list of players

--- src/guess-answers/guess-answers.jsx
+++ src/guess-answers/guess-answers.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Footer } from "../components/shared-footer";
 import { QuestionList } from "./question-list";
 import { PointHeader } from "./point-header";
+import { getAnswers } from "../apis/question-api";
+import { Context } from "../context";
 import "./guess-answers.css";
 
 const TOTAL_ROUNDS = 3;
 
 export function GuessAnswers() {
   const [round, setRound] = useState(1);
+  const [players, setPlayers] = useState([]);
+  const { user } = useContext(Context);
   const go = useNavigate();
+
+  useEffect(() => {
+    getAnswers(user.gameCode).then((data) =>
+      setPlayers(data.allAnswers.map((p) => p.playerName))
+    );
+  }, []);
 
   function handleRoundComplete() {
     if (round < TOTAL_ROUNDS) {
