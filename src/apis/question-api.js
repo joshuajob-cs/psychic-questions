5f6e3fabe06a8162b045b9ca6c37af610117fe8e
Joshua Job <joshuasjob@hotmail.com>
2026-03-10 16:21:52 -0600

    addAnswer  API

--- src/apis/question-api.js
+++ src/apis/question-api.js
@@ -1,3 +1,19 @@
+export async function addAnswer(gameCode, playerName, answer) {
+  const res = await fetch("/question/answer", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ gameCode, playerName, answer }),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error || "Could not add answer.");
+  }
+
+  return data;
+}
+
 export async function getQuestions() {
   const res = await fetch("/question");
 
