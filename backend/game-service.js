f7ecfff3f613a598f5058ce62fa84381c6906818
Joshua Job <joshuasjob@hotmail.com>
2026-03-09 23:28:23 -0600

    Can join with correct game code and name

--- backend/game-service.js
+++ backend/game-service.js
@@ -12,4 +12,17 @@ router.get("/check-code", (req, res) => {
   }
 });
 
+router.post("/join", (req, res) => {
+  const { gameCode, name } = req.body;
+  const game = games[gameCode];
+  if (!game) {
+    res.status(404).send({ msg: "Game not found" });
+  } else if (!game.addPlayer(name)) {
+    res.status(409).send({ msg: "Name already taken in this game" });
+  } else {
+    setSessionCookie(res, { username: null, name });
+    res.send({ name });
+  }
+});
+
 module.exports = router;
