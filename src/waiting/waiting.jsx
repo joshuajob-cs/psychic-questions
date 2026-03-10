c42eee11d6c64ca1c25a0221dccb6e939cd43664
Joshua Job <joshuasjob@hotmail.com>
2026-03-10 14:00:33 -0600

    Random facts api works now!

--- src/waiting/waiting.jsx
+++ src/waiting/waiting.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Title } from "../components/title";
 import { Footer } from "../components/shared-footer";
+import { getRandomFact } from "../apis/outsource-api";
 
 export function Waiting() {
   const navigate = useNavigate();
+  const [fact, setFact] = useState(null);
 
   useEffect(() => {
     const timer = setTimeout(() => navigate("/guess-answers"), 5000);
+    getRandomFact().then(setFact);
     return () => clearTimeout(timer);
   }, []);
 
@@ -20,9 +23,10 @@ export function Waiting() {
         <h1>Waiting...</h1>
         <img
           src="MysteriousWalking.jpg"
-          width="500"
+          width="400"
           alt="A person walking into a foggy, mysterious mist"
         />
+        {fact && <p>🔎 {fact} 🧠</p>}
       </main>
       <footer>
         <Footer />
