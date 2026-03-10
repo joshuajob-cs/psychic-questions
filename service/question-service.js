dd9db82ebf966cc167feb6f39692be5d193986de
Joshua Job <joshuasjob@hotmail.com>
2026-03-10 15:53:35 -0600

    Endpoint for client to grab list of questions. Once client and server share questions, questions can be reffered to by index

--- service/question-service.js
+++ service/question-service.js
@@ -3,4 +3,15 @@ import { games } from "./game-state.js";
 
 const router = express.Router();
 
+const questions = [
+  "Who do you aspire to be like?",
+  "What is your greatest fear?",
+  "What is something that is difficult for you to remember?",
+  "What do you think everyone should learn?",
+];
+
+router.get("/", (_req, res) => {
+  res.json({ questions });
+});
+
 export default router;
