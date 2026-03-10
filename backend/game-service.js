7cd4970575e0e01f5cf1869f0b5b4492dedb1081
Joshua Job <joshuasjob@hotmail.com>
2026-03-09 23:27:38 -0600

    Can check the game code

--- backend/game-service.js
+++ backend/game-service.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { games, Game } = require("./game-state");
+const { tokens, setSessionCookie } = require("./session-store");
+
+router.get("/check-code", (req, res) => {
+  const game = games[req.params.code];
+  if (game) {
+    res.send({});
+  } else {
+    res.status(404).send({ msg: "Game not found" });
+  }
+});
 
 module.exports = router;
