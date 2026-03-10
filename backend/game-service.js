4780ba9333a6ea509040ed39eb27bbd047f40fd1
Joshua Job <joshuasjob@hotmail.com>
2026-03-09 22:45:35 -0600

    Factored out game-state, so it can be used by both game-service, and question service

--- backend/game-service.js
+++ backend/game-service.js
@@ -1,29 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
-class Player {
-  constructor(name) {
-    this.name = name;
-    this.points = 0;
-    this.questions = [];
-  }
-
-  addQuestion(question, answer) {
-    this.questions.push({ question, answer });
-  }
-}
-
-class Game {
-  constructor(gameCode) {
-    this.gameCode = gameCode;
-    this.players = {};
-  }
-
-  addPlayer(username, name) {
-    this.players[username] = new Player(name);
-  }
-}
-
-const games = {};
+const { games, Game } = require("./game-state");
 
 module.exports = router;
--- backend/question-service.js
+++ backend/question-service.js
@@ -1,4 +1,5 @@
 const express = require("express");
 const router = express.Router();
+const { games } = require("./game-state");
 
 module.exports = router;
