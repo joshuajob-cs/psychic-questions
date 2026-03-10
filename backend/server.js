bf62536057763ac596681dcddedf11927202a192
Joshua Job <joshuasjob@hotmail.com>
2026-03-10 09:44:35 -0600

    Frontend served up using express middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 const authService = require("./auth-service");
 const gameService = require("./game-service");
@@ -8,7 +7,7 @@ const questionService = require("./question-service");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.static("../dist"));
 app.use(express.json());
 app.use(cookieParser());
 
