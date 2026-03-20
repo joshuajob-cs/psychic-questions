cdf11d34e293fa5fc6dd497afed65aed98035a91
Joshua Job <joshuasjob@hotmail.com>
2026-03-20 15:17:49 -0600

    Add collections to database

--- service/database/database.js
+++ service/database/database.js
@@ -4,8 +4,13 @@ import config from "./dbConfig.json" assert { type: "json" };
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
 const client = new MongoClient(url);
-const db = client.db("rental");
-const collection = db.collection("house");
+const db = client.db("psychic-questions");
+
+const userCollection = db.collection("users");
+const gameCollection = db.collection("games");
+const questionCollection = db.collection("questions");
+
+export { userCollection, gameCollection, questionCollection };
 
 async function main() {
   try {
