b5a0c5234a1fce6661a8c7e384331b87f3b65230
Joshua Job <joshuasjob@hotmail.com>
2026-03-20 16:26:27 -0600

    Add first test case

---
+++ service/test/auth-service.test.js
@@ -0,0 +1,25 @@
+import request from "supertest";
+import express from "express";
+import cookieParser from "cookie-parser";
+import authService from "../auth-service.js";
+import { client } from "../database/database.js";
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use("/auth", authService);
+
+afterAll(async () => {
+  await client.close();
+});
+
+describe("Auth Service", () => {
+  test("sign up a new user", async () => {
+    const res = await request(app)
+      .post("/auth/sign-up")
+      .send({ username: "testuser", password: "testpass" });
+
+    expect(res.status).toBe(200);
+    expect(res.body.username).toBe("testuser");
+  });
+});
