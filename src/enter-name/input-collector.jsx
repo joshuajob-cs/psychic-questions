96c6668cfd86a28e3f3bc99c4dc69533ee006640
Joshua Job <joshuasjob@hotmail.com>
2026-04-06 17:08:22 -0600

    When you enter a game send the message out

--- src/enter-name/input-collector.jsx
+++ src/enter-name/input-collector.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { InputForm } from "../components/input-form";
 import { Context } from "../context";
 import { joinGame } from "../apis/game-api";
+import { namesClient } from "../apis/websocket";
 
 export function InputCollector() {
   const { user, setUser } = useContext(Context);
@@ -15,6 +16,7 @@ export function InputCollector() {
         updateBackend={async (inputs) => {
           try {
             await joinGame(user.gameCode, inputs.name);
+            namesClient.sendName(inputs.name);
             return true;
           } catch (err) {
             return err.message;
