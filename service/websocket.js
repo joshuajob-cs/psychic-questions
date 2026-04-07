9a3ac5b56a56119674b94a9f71166a3cd27d6a4c
Joshua Job <joshuasjob@hotmail.com>
2026-04-06 18:49:27 -0600

    Connecting to the websocket should take gameCode as a parameter

--- service/websocket.js
+++ service/websocket.js
@@ -3,11 +3,14 @@ import { WebSocketServer } from "ws";
 export function startWebSocket(httpServer) {
   const wss = new WebSocketServer({ server: httpServer });
 
-  wss.on("connection", (ws) => {
+  wss.on("connection", (ws, req) => {
+    const url = new URL(req.url, "http://localhost");
+    ws.gameCode = url.searchParams.get("gameCode");
+
     ws.on("message", (data) => {
       const text = data.toString();
       wss.clients.forEach((client) => {
-        if (client !== ws && client.readyState === ws.OPEN) {
+        if (client !== ws && client.readyState === ws.OPEN && client.gameCode === ws.gameCode) {
           client.send(text);
         }
       });
