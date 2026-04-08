b6c99cd714fb17910d2087a43ce9be43e3590a1e
Joshua Job <joshuasjob@hotmail.com>
2026-04-07 18:16:50 -0600

    function create answer choices for each player

--- service/question-assigner.js
+++ service/question-assigner.js
@@ -1,6 +1,6 @@
 import questions from "./questions.js";
 
-const QUESTIONS_PER_PLAYER = 4;
+const QUESTIONS_PER_PLAYER = 5;
 
 function shuffle(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
@@ -8,6 +8,18 @@ function shuffle(arr) {
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
+}
+
+export function buildQuestionAnswerMap(players) {
+  const map = {};
+  for (const p of Object.values(players)) {
+    for (let i = 0; i < p.assignedQuestions.length; i++) {
+      const qi = p.assignedQuestions[i];
+      if (!map[qi]) map[qi] = {};
+      map[qi][p.name] = p.answers[i];
+    }
+  }
+  return map;
 }
 
 export function assignQuestionsToPlayers(players) {
