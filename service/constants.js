eb593871a5567ae04f8148eb10511a94b0440d49
Joshua Job <joshuasjob@hotmail.com>
2026-04-09 11:02:06 -0600

    Added system link

--- service/constants.js
+++ service/constants.js
@@ -1,9 +1,1 @@
-export const QUESTIONS_PER_PLAYER = 4;
-export const CHOICES_PER_QUESTION = 4;
-
-export const GamePhase = Object.freeze({
-  LOBBY: 'lobby',
-  ANSWERING: 'answering',
-  GUESSING: 'guessing',
-  WINNER: 'winner',
-});
+../shared/constants.js
--- shared/constants.js
+++ shared/constants.js
@@ -3,8 +3,8 @@ export const CHOICES_PER_QUESTION = 4;
 export const PLAYERS_PER_GUESSER = 4;
 
 export const GamePhase = Object.freeze({
-  LOBBY: 'lobby',
-  ANSWERING: 'answering',
-  GUESSING: 'guessing',
-  WINNER: 'winner',
+  LOBBY: "lobby",
+  ANSWERING: "answering",
+  GUESSING: "guessing",
+  WINNER: "winner",
 });
