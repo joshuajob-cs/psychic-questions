f3a1cae2019e4dffbffff150d05516f15fbd1063
Joshua Job <joshuasjob@hotmail.com>
2026-04-14 12:12:39 -0600

    Changed account anme to clarify meaning

--- src/components/auth-header.jsx
+++ src/components/auth-header.jsx
@@ -1,7 +1,9 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { Title } from "./title";
 
 export function AuthHeader() {
+  const { pathname } = useLocation();
+
   return (
     <header>
       <div className="screen-rotater rotater-gap">
@@ -15,24 +17,30 @@ export function AuthHeader() {
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
-              Account
+              New Game
             </button>
             <ul className="dropdown-menu" aria-labelledby="accountDropdown">
-              <li>
-                <NavLink className="dropdown-item" to="/">
-                  Join
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="dropdown-item" to="/login">
-                  Login
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="dropdown-item" to="/sign-up">
-                  Sign Up
-                </NavLink>
-              </li>
+              {pathname !== "/" && (
+                <li>
+                  <NavLink className="dropdown-item" to="/">
+                    Join
+                  </NavLink>
+                </li>
+              )}
+              {pathname !== "/login" && (
+                <li>
+                  <NavLink className="dropdown-item" to="/login">
+                    Login
+                  </NavLink>
+                </li>
+              )}
+              {pathname !== "/sign-up" && (
+                <li>
+                  <NavLink className="dropdown-item" to="/sign-up">
+                    Sign Up
+                  </NavLink>
+                </li>
+              )}
             </ul>
           </div>
         </nav>
