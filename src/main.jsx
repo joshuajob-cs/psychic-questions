e57c136b37a6efb7175a2e08a925ab8aa86c0a80
Joshua Job <joshuasjob@hotmail.com>
2026-05-30 22:28:07 -0600

    Deployment for FLy

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./src/app";
+import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
